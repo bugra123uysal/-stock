333f398e3b68aabca5d9ee5422d6563805f11901
buğra uysal <uysalbugra134@gmail.com>
2024-10-15 01:01:10 +0300

    Time series analysis of data started

--- pörtföy.py
+++ pörtföy.py
@@ -7,6 +7,8 @@ from bs4 import BeautifulSoup
 import yfinance as yf
 import random
 import tkinter as tk
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.statespace.sarimax import SARIMAX
 
 ana=tk.Tk()
 ana.title("hisse inceleme")
@@ -65,7 +67,7 @@ tt=pd.DataFrame(prct).T
 print(tt)
 
 
-
+"""  
 sns.barplot(x=tt.index , y=tt['yüzde değişimi'], palette='viridis')
 plt.xticks(rotation=90)
 plt.show()
@@ -75,4 +77,49 @@ sns.lineplot(x=tt.index, y=tt['bu gün'], palette='viridis')
 sns.lineplot(x=tt.index, y=tt['yıl bası'], palette='viridis')
 plt.show()
 
+"""
+
+
+
+
 ana.mainloop()
+
+sto='TSLA'
+ff=yf.download(sto, start='2022-01-01', end='2024-01-01')
+
+kapaa=ff['Close']
+
+
+
+
+plt.figure(figsize=(10,6))
+plt.plot(kapaa.index, kapaa, label='Geçmiş Fiyat')
+
+forecast_data=pd.date_range(start=kapaa.index[-1],periods=180, freq='D')
+plt.title(f'{sto} 6 ay sonrası ')
+plt.xlabel('Tarih')
+plt.ylabel('fiyat')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+mdo=ARIMA(kapaa,order=(5, 1, 0))
+egıt=mdo.fit()
+
+
+forecast, stderr,conf_int=egıt.forecast(steps=180)
+forecast_data=pd.date_range(start=kapaa.index[-1],periods=180, freq='D')
+
+
+
+plt.plot(forecast_data, forecast, label="tahmin edilen fiyat", color='red')
+plt.fill_between(forecast_data, conf_int[:, 0], conf_int[:, 1],color='gray', alpha=0.3 )
+
+plt.title(f'{sto} 6 Ay Sonrası Fiyat Tahmini (ARIMA)')
+plt.xlabel('Tarih')
+plt.ylabel('Fiyat (USD)')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+
